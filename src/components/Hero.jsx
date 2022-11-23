764abcb895b9cead6d771a8940738cb9f467820d
Sephens <adesephens@gmail.com>
2022-11-23 15:13:48 +0300

    day 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
-import meme1 from '../assets/meme1.jpg'
+import React from "react";
+import meme1 from "../assets/meme1.jpg";
+
+import memes from '../../src/memes'
 
 export default function Hero() {
-  return (
-    <div className='hero'>
 
-        <div className="container">
+  let url
 
-        <div className="top-hero">
-
-            <input type="text" placeholder='Shut up' id='left-input'/>
-            <input type="text" placeholder='and take money' id='right-input'/>
-
-        </div>
-
-        <div className="middle-hero">
-
-            <a href="#" id='get-meme-btn'>Get a new meme image</a>
-
-        </div>
-
-        <div className="bottom-hero">
-            <img src={meme1} alt="" id='meme'/>
-
-        </div>
-
-        </div>
+  function handleClick(event){
+    event.preventDefault();
+    const memesArray = memes.data.memes
+    const randomMeme = Math.floor(Math.random() * memesArray.length)
+    url = memesArray[randomMeme].url
+    return(
       
+      console.log(url)
+    )
+  }
+  return (
+    <div className="hero">
+      <div className="container">
+      <div className="form">
+        <form className="gen-form">
+        <div className="top-form">
+          <input type="text" placeholder="Shut up" id="left-input" />
+          <input type="text" placeholder="and take money" id="right-input" />
+
+          </div>
+
+          <div className="btn">
+          <button id="get-meme-btn" onClick={handleClick}>Get a new meme image</button>
+          </div>
+        </form>
+
+        <div className="meme-image">
+        <img src={meme1} alt="" id='meme'/>
+        </div>
+      </div>
+      </div>
     </div>
-  )
+  );
 }
